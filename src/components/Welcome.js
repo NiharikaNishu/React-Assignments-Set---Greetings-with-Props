7c7c0adef026d68dfdc84cc47e34aab131b6d32a
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-12-06 16:18:43 +0530

    sve

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,11 @@
 // write code for Welcome component here
-  
+
 import React from "react";
-export default function (props) {
-  const { name } = props;
+export default function () {
+  // const { name } = props;
   return (
     <>
-      <h1>Hey {name}!</h1>
+      <h1>Hey!</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
